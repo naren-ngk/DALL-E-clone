c714438318ea152db7c1c88dc4917f73cce9565e
naren-ngk <narensakthiit@gmail.com>
2023-05-07 20:22:14 +0530

    Updatec commit

--- server/routes/postRoutes.js
+++ server/routes/postRoutes.js
@@ -1,7 +1,9 @@
 import express from 'express';
 import * as dotenv from 'dotenv';
-import { Configuration,OpenAIApi } from 'openai';
+import { v2 as cloudinary } from 'cloudinary';
 
 dotenv.config();
 
 const router = express.Router();
+
+export default router;
